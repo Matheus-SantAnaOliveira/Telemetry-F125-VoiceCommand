d0a4b9a134f6bc0eaa51e0369823956cccdfe756
Matheus Sant'Ana <Matheussantana3105@gmail.com>
2026-01-04 23:21:14 -0300

    Adicionando informações precisas sobre cada pneu(temperatura, dano, dano nos freios

--- Api-Telemetry-F1/Models/CarDamage/CarDamageData.cs
+++ Api-Telemetry-F1/Models/CarDamage/CarDamageData.cs
@@ -3,9 +3,25 @@
     public class CarDamageData
     {
         public float[] TyresWear { get; set; } = new float[4];
+        public float TyreWearFrontLeft { get; set; }
+        public float TyreWearFrontRight { get; set; }
+        public float TyreWearRearLeft { get; set; }
+        public float TyreWearRearRight { get; set; }
         public byte[] TyresDamage { get; set; } = new byte[4];
+        public byte TyreDamageFrontLeft { get; set; }
+        public byte TyreDamageFrontRight { get; set; }
+        public byte TyreDamageRearLeft { get; set; }
+        public byte TyreDamageRearRight { get; set; }
         public byte[] BrakesDamage { get; set; } = new byte[4];
+        public byte BrakeDamageFrontLeft { get; set; }
+        public byte BrakeDamageFrontRight { get; set; }
+        public byte BrakeDamageRearLeft { get; set; }
+        public byte BrakeDamageRearRight { get; set; }
         public byte[] TyreBlisters { get; set; } = new byte[4];
+        public byte TyreBlisterFrontLeft { get; set; }
+        public byte TyreBlisterFrontRight { get; set; }
+        public byte TyreBlisterRearLeft { get; set; }
+        public byte TyreBlisterRearRight { get; set; }
         public byte FrontLeftWingDamage { get; set; }
         public byte FrontRightWingDamage { get; set; }
         public byte RearWingDamage { get; set; }
